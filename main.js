3dc73f50e76417ac757c04379b5d76edc25daafb
juan hoyos <juanestebanarevalo28@gmail.com>
2025-02-15 14:38:53 -0500

    1000

--- main.js
+++ main.js
@@ -1,72 +1,77 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-  
-
-    dibujarMapa()
-    buscarIp()
+document.addEventListener("DOMContentLoaded", function () {
+  dibujarMapa();
+  buscarIp();
 });
 
-function buscarIp(){
-  let boton = document.getElementById('buscar')
-  boton.addEventListener('click', ()=>{
-    ip= document.getElementById('ipBuscar').value.trim()
-    dibujarMapa(ip)
-  })
+function buscarIp() {
+  let boton = document.getElementById("buscar");
+  boton.addEventListener("click", () => {
+    ip = document.getElementById("ipBuscar").value.trim();
+    dibujarMapa(ip);
+  });
 }
 let map;
 
-
 function dibujarMapa(ip) {
   if (!ip) {
-    ip = ""
+    ip = "";
   }
-  obtenerDatosIp(ip).then(data => {
-    if (data.status!="success") {
-      alert("ip desconocida")
-    }else{
+  obtenerDatosIp(ip).then((data) => {
+    if (data.status != "success") {
+      alert("ip desconocida");
+    } else {
       const options = {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZone: data.timezone
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZone: data.timezone,
       };
-      
+
       let date = new Date();
-      let hora = new Intl.DateTimeFormat('en-US', options).format(date);
-      
-      dibujarResultado(data.query, data.city, data.region, data.zip, hora, data.isp)
-     
-      
-        const lat = data.lat;
-          const lon = data.lon;
-          
-          if (map) {
-            map.remove();
-          }
-          map = L.map("map").setView([lat, lon], 10);
-    
-          L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);
-    
-          L.marker([lat, lon]).addTo(map).bindPopup(`${data.city}, ${data.countryCode}`).openPopup();
+      let hora = new Intl.DateTimeFormat("en-US", options).format(date);
+
+      dibujarResultado(
+        data.query,
+        data.city,
+        data.region,
+        data.zip,
+        hora,
+        data.isp
+      );
+
+      const lat = data.lat;
+      const lon = data.lon;
+
+      if (map) {
+        map.remove();
+      }
+      map = L.map("map").setView([lat, lon], 10);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup(`${data.city}, ${data.countryCode}`)
+        .openPopup();
     }
   });
-  
-  
 }
 
 function dibujarResultado(ip, lugar, region, postal, hora, isp) {
-  document.getElementById('ip').textContent = ip
-  document.getElementById('lugar').textContent = lugar+","+ region +" " +postal
-  document.getElementById('hora').textContent = hora
-  document.getElementById('isp').textContent = isp
-  document.getElementById('ipBuscar').value = ip
+  document.getElementById("ip").textContent = ip;
+  document.getElementById("lugar").textContent =
+    lugar + "," + region + " " + postal;
+  document.getElementById("hora").textContent = hora;
+  document.getElementById("isp").textContent = isp;
+  document.getElementById("ipBuscar").value = ip;
 }
 
 async function obtenerDatosIp(ip) {
-  const response = await fetch(`http://ip-api.com/json/${ip}`);
+  const response = await fetch(`https://ip-api.com/json/${ip}`);
   if (!response.ok) {
-      throw new Error('Error al obtener los datos');
+    throw new Error("Error al obtener los datos");
   }
   return response.json();
 }
